89b6b93ccd7b09b3347d4b700142dac6cfddd116
Julian Osorio <je.osoriol@uniandes,edu.co>
2017-10-01 13:50:02 -0500

    Iter 4 - Paso 2 (Rojo), Se agrega prueba para probar con un número.

--- EstadisticasTest4.py
+++ EstadisticasTest4.py
@@ -4,3 +4,6 @@ from Estadisticas import Estadisticas
 class EstadisticasTest4(TestCase):
     def test_calcular(self):
         self.assertEqual(Estadisticas().calcular(""), [0, None, None, None], "Cadena Vacia Iter 4")
+
+    def test_calcularUnNumero(self):
+        self.assertEqual(Estadisticas().calcular("11"), [1, 11, 11, 11], "Un Numero Iter 4")
