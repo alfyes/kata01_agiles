be3f2ce2d9219ed286a40c337b014bc31551e30f
Alfonso Ardila <alfyes@hotmail.com>
2017-10-01 13:19:30 -0500

    Iter 2 - Paso 4 (Rojo), Se agrega prueba para probar con cadena con múltiples números

--- EstadisticasTest2.py
+++ EstadisticasTest2.py
@@ -10,3 +10,6 @@ class EstadisticasTest2(TestCase):
 
     def test_calcularDosNumeros(self):
         self.assertEqual(Estadisticas().calcular("11,5"), [2, 5], "Dos Numero Iter 2")
+
+    def test_calcularMultiplesNumeros(self):
+        self.assertEqual(Estadisticas().calcular("11,5,8,2"), [4, 2], "Multiples Numeros Iter 2")
