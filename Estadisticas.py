0bedd83776313165d013709c904ebab028220277
Julian Osorio <je.osoriol@uniandes,edu.co>
2017-10-01 13:02:29 -0500

    Iter 2 - Paso 1 (Verde), Se modifica método calcular para que pase el test para cadena vacia.

--- Estadisticas.py
+++ Estadisticas.py
@@ -1,7 +1,7 @@
 class Estadisticas:
     def calcular(self, cadena):
         if cadena == "":
-            return [0]
+            return [0, None]
         elif "," in cadena:
             numeros = cadena.split(",")
             return [len(numeros)]
