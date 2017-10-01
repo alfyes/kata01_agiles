8c372686445304310f316c9566383ecc03b87b78
Alfonso Ardila <alfyes@hotmail.com>
2017-10-01 13:54:43 -0500

    Iter 4 - Paso 3 (Rojo), Se agrega prueba para probar con dos números.

--- EstadisticasTest4.py
+++ EstadisticasTest4.py
@@ -7,3 +7,6 @@ class EstadisticasTest4(TestCase):
 
     def test_calcularUnNumero(self):
         self.assertEqual(Estadisticas().calcular("11"), [1, 11, 11, 11], "Un Numero Iter 4")
+
+    def test_calcularDosNumeros(self):
+        self.assertEqual(Estadisticas().calcular("11,5"), [2, 5, 11, 8], "Dos Numeros Iter 4")
