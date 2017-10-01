8cc8fdcacc57e640f528161d564232f6ec2a5276
Alfonso Ardila <alfyes@hotmail.com>
2017-10-01 13:31:27 -0500

    Iter 3 - Paso 2 (Rojo), Se agrega prueba para probar con un número.

--- EstadisticasTest3.py
+++ EstadisticasTest3.py
@@ -5,3 +5,6 @@ from Estadisticas import Estadisticas
 class EstadisticasTest3(TestCase):
     def test_calcular(self):
         self.assertEqual(Estadisticas().calcular(""), [0, None, None], "Cadena Vacia Iter 3")
+
+    def test_calcularUnNumero(self):
+        self.assertEqual(Estadisticas().calcular("11"), [1, 11, 11], "Un Numero Iter 3")
